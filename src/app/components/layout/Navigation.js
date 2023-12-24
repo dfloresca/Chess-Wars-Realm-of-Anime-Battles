05adfec9352fe15f7cc0f86bf5f78108d0fa42c8
susan stephens <crazeygrafix@gmail.com>
2023-12-23 19:43:24 -0700

    navigation nearly complete

---
+++ src/app/components/layout/Navigation.js
@@ -0,0 +1,44 @@
+import Link from 'next/link';
+import styles from './Navigation.module.css'
+
+
+export default function Navigation() {
+    return (
+
+        <>
+            <nav className={styles.mainNav}>
+
+                <ul className={styles.leftLinks}>
+                    <Link href="/"><img className={styles.logo} src="/Floresca Threads Logo.png" /></Link>
+                    {/* <li><Link href="/">Home</Link> </li> */}
+                    <li><Link href="/products">Products</Link> </li>
+                    <li><Link href="/customize">Customize</Link> </li>
+                    <li><Link href="/about">About Us</Link> </li>
+                </ul>
+
+
+                <ul className={styles.rightLinks}>
+                    <li className={styles.svgLinks}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                    </svg>
+                    </li>
+                    <li className={styles.svgLinks}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+                    </li>
+                    <li className={styles.svgLinks}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                    </svg>
+                    </li>
+                    <li className={styles.svgLinks}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                    </li>
+                </ul>
+
+            </nav>
+
+        </>
+
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -4,34 +4,37 @@ import Image from 'next/image';
 import styles from './page.module.css';
 import { useEffect, useState } from 'react';
 import setAuthToken from './utils/setAuthToken';
+import Navigation from './components/layout/navigation';
 
 // we are going to be fetching data from our API and displaying it on
 // the page
 
 export default function Home() {
   // state is what the data is representing in realtime
-  const [data, setData] = useState(null);
-  const [isLoading, setLoading] = useState(false);
-  const [age, setAge] = useState(null);
-  const [name, setName] = useState('Dylan');
+  // const [data, setData] = useState(null);
+  // const [isLoading, setLoading] = useState(false);
+  // const [age, setAge] = useState(null);
+  // const [name, setName] = useState('Dylan');
 
-  useEffect(() => {
-    // hitting an api and setting our component with some data
-    fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/`)
-      .then((res) => res.json())
-      .then((data) => {
-        // data is an object
-        setData(data);
-        setLoading(false);
-      });
-  }, []);
+  // useEffect(() => {
+  //   // hitting an api and setting our component with some data
+  //   fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       // data is an object
+  //       setData(data);
+  //       setLoading(false);
+  //     });
+  // }, []);
 
-  if (isLoading) return <p>Loading...</p>;
-  if (!data) return <p>No data shown...</p>;
+  // if (isLoading) return <p>Loading...</p>;
+  // if (!data) return <p>No data shown...</p>;
 
   return (
     <main className={styles.main}>
-      <p>{data.message}</p>
+      {/* <p>{data.message}</p> */}
+      <Navigation />
+      <h1>welcome to the homepage</h1>
     </main>
   );
 }
