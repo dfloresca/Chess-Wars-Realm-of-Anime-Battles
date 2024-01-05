2b131c97e3cdf655ea7e15974a32b64a5f8d19ce
Daniel Floresca <dfloresca@gmail.com>
2024-01-05 15:53:31 -0800

    feat updated contact and contact.module.css

--- src/app/components/contactUs.js
+++ src/app/components/contactUs.js
@@ -1,24 +1,127 @@
-import styles from "../contact/contact.module.css"
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Form, Button } from 'react-bootstrap';
+import styles from "../contact/contact.module.css";
+
 export default function ContactUs() {
+    const [state, setState] = useState({
+        name: "",
+        email: "",
+        subject: "",
+        message: ""
+    });
+
+    const [result, setResult] = useState(null);
+
+
+    const sendEmail = event => {
+        event.preventDefault();
+        axios
+            .post(`${process.env.NEXT_PUBLIC_SERVER_URL}/send`, { ...state })
+            .then(response => {
+                setResult(response.data);
+                setState({
+                    name: '', 
+                    email: '', 
+                    subject: '', 
+                    message: '' 
+                });
+            })
+            .catch(() => {
+                setResult({ success: false, message: 'Something went wrong. Try again later'});
+            })
+        
+    }
+
+    const onInputChange = event => {
+        const { name, value } = event.target;
+
+        setState({
+            ...state,
+            [name]: value
+        });
+    };
+
     return (
         <div className={styles.formDiv}>
+            {result && (
+                <p className={`${result.success ? 'success' : 'error'}`}>
+                    {result.message}
+                </p>
+            )}
             <h1 className={styles.messageUs}>Send us a Message</h1>
-            <form className={styles.contactForm}>
-                <div className={styles.fullName}>
-                    <label htmlFor="firstName">First Name</label>
-                    <input name="firstName" type="text" id="firstName" />
-                    <label htmlFor="lastName">Last Name</label>
-                    <input name="lastName" type="text" id="lastName" />
-                </div>
-                <label htmlFor="emailAddress">Email Address</label>
-                <input name="emailAddress" type="text" id="emailAddress" />
-                <label htmlFor="message">Message</label>
-                <textarea name="message" type="text" id="message" rows={5} />
+            <form className={styles.contactForm} onSubmit={sendEmail}>
+                <Form.Group controlId="name">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="name"
+                        value={state.name}
+                        placeholder="Enter your full name"
+                        onChange={onInputChange}
+                    />
+                </Form.Group>
+                <Form.Group controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="email"
+                        value={state.email}
+                        placeholder="Enter your email"
+                        onChange={onInputChange}
+                    />
+                </Form.Group>
+                <Form.Group controlId="subject">
+                    <Form.Label>Subject</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="subject"
+                        value={state.subject}
+                        placeholder="Enter subject"
+                        onChange={onInputChange}
+                    />
+                </Form.Group>
+                <Form.Group controlId="message">
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        name="message"
+                        value={state.message}
+                        rows="3"
+                        placeholder="Enter your message"
+                        onChange={onInputChange}
+                    />
+                </Form.Group>
                 <div className={styles.buttons}>
-                    <button type="submit" >Send</button>
-                    <button type='reset' >Clear</button>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
                 </div>
             </form>
         </div>
     )
 }
+
+
+// return (
+//     <div className={styles.formDiv}>
+//         <h1 className={styles.messageUs}>Send us a Message</h1>
+//         <form className={styles.contactForm}>
+//             {/* <div className={styles.fullName}> */}
+//                 <label htmlFor="fullName">Full Name</label>
+//                 <input placeholder="Please enter Full Name" name="fullName" type="text" id="fullName" />
+//                 <label htmlFor="emailAddress">Email Address</label>
+//                 <input placeholder="Please enter Email Address" name="emailAddress" type="email" id="emailAddress" />
+//             {/* </div> */}
+//             <label htmlFor="subject">Subject</label>
+//             <input placeholder="Enter subject here..." name="subject" type="text" id="subject" />
+//             <label htmlFor="message">Message</label>
+//             <textarea name="message" type="text" id="message" rows={5} />
+//             <div className={styles.buttons}>
+//                 <button type="submit" >Send</button>
+//                 <button type='reset' >Clear</button>
+//             </div>
+//         </form>
+//     </div>
+// )
+// }
