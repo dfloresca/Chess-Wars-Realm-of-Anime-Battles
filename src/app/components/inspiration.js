474c4e7742feec56acc8c9b001fc3b90c716bc4b
Daniel Floresca <dfloresca@gmail.com>
2024-01-05 17:05:11 -0800

    feat updated signup.js

--- src/app/components/inspiration.js
+++ src/app/components/inspiration.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Inspiration() {
     return (
         <div className={styles.inspiration}>
-            <Image src='/happyfamily.png' alt='happy family' width='350' height='350' />
+            <Image src='/happyfamily.png' priority={true} alt='happy family' width='350' height='350' />
             <h2>
                 Family fun
             </h2>
